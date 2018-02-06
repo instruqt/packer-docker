d23572eb455a1f6186029f17fe2e18acf0d052f2
Bas Tichelaar <bas@tichelaar.io>
2018-02-06 16:43:48 +0100

    Added docker-compose

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 export DEBIAN_FRONTEND=noninteractive 
 
 apt-get update && apt-get -y upgrade
@@ -9,19 +11,24 @@ apt-get -y install \
     ca-certificates \
     curl \
     sudo \
-    unzip \
     vim \
     nano \
-    joe \
-    emacs \
-    gnupg2 \
     software-properties-common
 
+# Install Docker
 curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
-
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
    $(lsb_release -cs) \
    stable"
 
 apt-get update && apt-get install -y docker-ce
+
+# Install Docker Compose
+curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+
+# Improve the startup sequence
+cp /tmp/resources/google-startup-scripts.service /etc/systemd/system/multi-user.target.wants/google-startup-scripts.service
+systemctl daemon-reload
