cf628e8883f35bfb3ed54a7c50ca6d352b00833d
Adé Mochtar <ade@instruqt.com>
2019-11-20 09:38:32 +0100

    Update to docker 19.03

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-set -e
+set -ex
 
-export DEBIAN_FRONTEND=noninteractive 
+export DEBIAN_FRONTEND=noninteractive
 
 apt-get update && apt-get -y upgrade
 apt-get -y install \
@@ -13,22 +13,10 @@ apt-get -y install \
     sudo \
     vim \
     nano \
-    software-properties-common
+    software-properties-common \
+    docker.io
 
-# Install Docker
-curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
-   $(lsb_release -cs) \
-   stable"
-
-apt-get update && apt-get install -y docker-ce
 
 # Install Docker Compose
-curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-
-
-# Improve the startup sequence
-cp /tmp/resources/google-startup-scripts.service /etc/systemd/system/multi-user.target.wants/google-startup-scripts.service
-systemctl daemon-reload
